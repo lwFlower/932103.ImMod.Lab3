b392b98ee8388a271558898d20f537d43149e3fc
Arina <144441111+lwFlower@users.noreply.github.com>
2024-03-02 19:18:40 +0700

    Add files via upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btResult = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.masked = new System.Windows.Forms.MaskedTextBox();
+            this.btClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btClear);
+            this.panel1.Controls.Add(this.btResult);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.masked);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(705, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 503);
+            this.panel1.TabIndex = 0;
+            // 
+            // btResult
+            // 
+            this.btResult.Location = new System.Drawing.Point(17, 69);
+            this.btResult.Name = "btResult";
+            this.btResult.Size = new System.Drawing.Size(121, 36);
+            this.btResult.TabIndex = 2;
+            this.btResult.Text = "Результат";
+            this.btResult.UseVisualStyleBackColor = true;
+            this.btResult.Click += new System.EventHandler(this.btResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер правила";
+            // 
+            // masked
+            // 
+            this.masked.Location = new System.Drawing.Point(23, 28);
+            this.masked.Mask = "000";
+            this.masked.Name = "masked";
+            this.masked.ResetOnSpace = false;
+            this.masked.Size = new System.Drawing.Size(106, 22);
+            this.masked.TabIndex = 0;
+            this.masked.TextChanged += new System.EventHandler(this.masked_TextChanged);
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(17, 133);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(121, 36);
+            this.btClear.TabIndex = 3;
+            this.btClear.Text = "Очистить";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 503);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox masked;
+        private System.Windows.Forms.Button btResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        int sizeCells = 4;
+        int[,] fieldColors;
+        int countRows = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            ClientSize = new Size(750, 600);
+            fieldColors = new int[(ClientSize.Width - 150) / sizeCells, ClientSize.Height / sizeCells];
+            MouseDown += Draw;
+            DoubleBuffered = true;
+        }
+
+        private void Draw(object sender, MouseEventArgs e)
+        {
+            var thisX = e.X;
+            fieldColors[0, thisX / sizeCells] = fieldColors[0, thisX / sizeCells] == 1 ? 0 : 1;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            InitCells(e.Graphics);
+            InitGrid(e.Graphics);
+        }
+
+        private void InitGrid(Graphics g)
+        {
+            for (int i = 0; i <= countRows; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(0, sizeCells * i), new Point(ClientSize.Width - 150, sizeCells * i));
+            }
+            for (int i = 0; i <= (ClientSize.Width - 150) / sizeCells; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(sizeCells * i, 0), new Point(sizeCells * i, sizeCells * countRows));
+            }
+        }
+
+        private void InitCells(Graphics g)
+        {
+            for (int i = 0; i < countRows; i++)
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / sizeCells; j++)
+                {
+                    g.DrawLine(new Pen(fieldColors[i, j] == 1 ? Color.Black : Color.White, sizeCells), sizeCells * j, sizeCells * i + sizeCells / 2, sizeCells * j + sizeCells, sizeCells * i + sizeCells / 2);
+                }
+            }
+        }
+        private void masked_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.Parse(masked.Text) > 255)
+                {
+                    masked.Text = "255";
+                }
+            }
+            catch { }
+        }
+        string Converter()
+        {
+            var binarySys = "";
+            var decimalSys = int.Parse(masked.Text);
+            while (decimalSys >= 2)
+            {
+                binarySys = decimalSys % 2 + binarySys;
+                decimalSys /= 2;
+            }
+
+            if (decimalSys == 1) binarySys = 1 + binarySys;
+
+            var tempLength = binarySys.Length;
+            for (int i = 0; i < 8 - tempLength; i++)
+            {
+                binarySys = 0 + binarySys;
+            }
+            return binarySys;
+        }
+        private void btResult_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            irows = 1;
+            for (int i = 0; i < countRows; i++)
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / sizeCells; j++)
+                {
+                    fieldColors[i, j] = 0;
+                }
+            }
+            Invalidate();
+        }
+        int irows = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var rule = Converter();
+            countRows = ClientSize.Height / sizeCells;
+            if ((irows < countRows) && (timer1.Enabled == true))
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / sizeCells; j++)
+                {
+                    var left = j - 1;
+                    var right = j + 1;
+                    if (j == (ClientSize.Width - 150) / sizeCells - 1)
+                    {
+                        right = 0;
+                    }
+                    if (j == 0)
+                    {
+                        left = (ClientSize.Width - 150) / sizeCells - 1;
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "111")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[0].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "110")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[1].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "101")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[2].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "100")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[3].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "011")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[4].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "010")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[5].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "001")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[6].ToString());
+                    }
+                    if (fieldColors[irows - 1, left].ToString() + fieldColors[irows - 1, j].ToString() + fieldColors[irows - 1, right].ToString() == "000")
+                    {
+                        fieldColors[irows, j] = int.Parse(rule[7].ToString());
+                    }
+                }
+                Invalidate();
+                irows += 1;
+            }
+            else
+            {
+                timer1.Stop();
+                irows = 1;
+            }
+        }
+    }
+}
